117fac7f5c426cdcf7ebe48ba34e5a19c45b0887
doora <dorasyu1202@gmail.com>
2025-10-29 14:12:33 +0900

    update

--- pcdet/__init__.py
+++ pcdet/__init__.py
@@ -9,12 +9,14 @@ __all__ = [
 
 
 def get_git_commit_number():
-    if not (Path(__file__).parent / '../.git').exists():
-        return '0000000'
 
-    cmd_out = subprocess.run(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE)
-    git_commit_number = cmd_out.stdout.decode('utf-8')[:7]
-    return git_commit_number
+    return '0000000'
+    # if not (Path(__file__).parent / '../.git').exists():
+    #     return '0000000'
+
+    # cmd_out = subprocess.run(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE)
+    # git_commit_number = cmd_out.stdout.decode('utf-8')[:7]
+    # return git_commit_number
 
 
 script_version = get_git_commit_number()
